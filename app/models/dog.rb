eed784a0259ecd659a11c767f6e739c2d6355086
Laurent Gardin <laurent.gardin@laposte.net>
2026-06-02 16:41:23 +0200

    maj conversation/dog.rb

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,3 +1,6 @@
 class Dog < ApplicationRecord
   belongs_to :user
+
+  has_many :conversations_as_one, class_name: "Conversation", foreign_key: "dog_one_id", dependent: :destroy
+  has_many :conversations_as_two, class_name: "Conversation", foreign_key: "dog_two_id", dependent: :destroy
 end
