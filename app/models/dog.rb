28d02520bf31fcf4233be9ba5b2cf152ad98dff4
Remi Chaput <remi.chpt@gmail.com>
2026-06-03 12:41:24 +0200

    remi_mcv_dog_navbar

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,3 +1,13 @@
 class Dog < ApplicationRecord
   belongs_to :user
+
+  validates :name, presence: true, length: { minimum: 2, maximum: 30 }
+
+  validates :breed, presence: true
+
+  validates :description, presence: true, length: { minimum: 12, maximum: 300 }
+
+  validates :age, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than: 30 }
+
+  validates :user, presence: true
 end
