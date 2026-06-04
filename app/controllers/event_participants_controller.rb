938000a61f0cec9d545bb4d90a8b1b8180cbaae1
Rindrasoa Rasamoelina <rindramiangaly@gmail.com>
2026-06-04 12:54:04 +0200

    Ajout évènements canins

--- app/controllers/event_participants_controller.rb
+++ app/controllers/event_participants_controller.rb
@@ -1,7 +1,16 @@
 class EventParticipantsController < ApplicationController
-  def create
-  end
+  before_action :authenticate_user!
 
-  def destroy
+  def create
+    @event = Event.find(params[:event_id])
+    @participant = EventParticipant.new
+    @participant.event = @event
+    @participant.dog = current_user.dogs.first
+
+    if @participant.save
+      redirect_to event_path(@event), notice: "Ton chien est inscrit à l'événement."
+    else
+      redirect_to event_path(@event), alert: "Ton chien est déjà inscrit."
+    end
   end
 end
--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,22 +1,33 @@
 class EventsController < ApplicationController
+  before_action :authenticate_user!
+
   def index
+    @events = Event.order(date: :asc)
   end
 
   def show
+    @event = Event.find(params[:id])
+    @participant = EventParticipant.new
   end
 
   def new
+    @event = Event.new
   end
 
   def create
+    @event = Event.new(event_params)
+    @event.dog = current_user.dogs.first
+
+    if @event.save
+      redirect_to event_path(@event)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
-  def edit
-  end
+  private
 
-  def update
-  end
-
-  def destroy
+  def event_params
+    params.require(:event).permit(:title, :description, :city, :date)
   end
 end
