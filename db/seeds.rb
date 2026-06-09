0e65edfbf04c65a640e974635bac3b423274bdab
Rindrasoa Rasamoelina <rindramiangaly@gmail.com>
2026-06-10 01:31:27 +0200

    Sauvegarde avancement front matching NE PAS METTRE SUR MASTER

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,118 +1,85 @@
+require "open-uri"
+
 puts "Cleaning database..."
 
+Wouf.destroy_all if defined?(Wouf)
+Comment.destroy_all if defined?(Comment)
+Post.destroy_all if defined?(Post)
 Swipe.destroy_all if defined?(Swipe)
-AiMessage.destroy_all
-AiChat.destroy_all
-Message.destroy_all
-Conversation.destroy_all
-Comment.destroy_all
-Wouf.destroy_all
-Post.destroy_all
+Message.destroy_all if defined?(Message)
+Conversation.destroy_all if defined?(Conversation)
+AiMessage.destroy_all if defined?(AiMessage)
+AiChat.destroy_all if defined?(AiChat)
 EventParticipant.destroy_all if defined?(EventParticipant)
 Event.destroy_all if defined?(Event)
 Dog.destroy_all
 User.destroy_all
 
-puts "Creating users..."
+users_data = [
+  ["Emma", "Martin", "emma"],
+  ["Lucas", "Bernard", "lucas"],
+  ["Chloé", "Petit", "chloe"],
+  ["Nathan", "Durand", "nathan"],
+  ["Sarah", "Moreau", "sarah"],
+  ["Thomas", "Laurent", "thomas"],
+  ["Julie", "Simon", "julie"],
+  ["Hugo", "Michel", "hugo"],
+  ["Camille", "Lefevre", "camille"],
+  ["Alexandre", "Roux", "alexandre"]
+]
 
-users = []
+dogs_data = [
+  ["Rex", "Golden Retriever"], ["Bella", "Labrador"],
+  ["Max", "Beagle"], ["Luna", "Border Collie"],
+  ["Rocky", "Berger Allemand"], ["Milo", "Husky"],
+  ["Nala", "Cocker"], ["Simba", "Dalmatien"],
+  ["Daisy", "Caniche"], ["Buddy", "Boxer"],
+  ["Oscar", "Shiba Inu"], ["Ruby", "Spitz"],
+  ["Coco", "Bouledogue Français"], ["Leo", "Akita"],
+  ["Maya", "Berger Australien"], ["Jack", "Teckel"],
+  ["Lucky", "Bichon"], ["Nova", "Malinois"],
+  ["Tina", "Carlin"], ["Ollie", "Corgi"]
+]
 
-6.times do |i|
-  users << User.create!(
-    email: "user#{i + 1}@example.com",
-    password: "password",
-    username: "user#{i + 1}",
-    first_name: "User#{i + 1}",
-    last_name: "Doglover"
+dog_index = 0
+
+users_data.each do |first_name, last_name, username|
+  user = User.create!(
+    first_name: first_name,
+    last_name: last_name,
+    username: username,
+    email: "#{username}@doggy.com",
+    password: "password123"
   )
-end
 
-puts "Creating dogs..."
+  2.times do
+    name, breed = dogs_data[dog_index]
 
-sizes = ["Petit", "Moyen", "Grand"]
-energy_levels = ["Très calme", "Calme", "Équilibré", "Dynamique", "Très dynamique"]
-canine_sociabilities = ["Très réservé", "Sélectif", "Sociable", "Très sociable", "Adore tous les chiens"]
-human_sociabilities = ["Méfiant", "Réservé", "Amical", "Très affectueux", "Adore les humains"]
-temperaments = ["Joueur", "Protecteur", "Aventurier", "Affectueux", "Indépendant"]
-favorite_activities = ["Promenade tranquille", "Jeux", "Course", "Randonnée", "Baignade"]
-
-breeds = [
-  "Golden Retriever",
-  "Husky",
-  "Labrador",
-  "Berger Allemand",
-  "Cocker",
-  "Beagle",
-  "Border Collie",
-  "Shiba Inu",
-  "Bulldog",
-  "Caniche"
-]
-
-dog_names = [
-  "Buddy", "Luna", "Max", "Bella", "Rocky",
-  "Nala", "Oscar", "Milo", "Ruby", "Simba"
-]
-
-users.each_with_index do |user, user_index|
-  10.times do |i|
-    Dog.create!(
+    dog = Dog.create!(
       user: user,
-      name: "#{dog_names[i]} #{user_index + 1}",
-      breed: breeds[i],
-      age: rand(1..12),
-      description: "Chien #{temperaments.sample.downcase}, qui aime #{favorite_activities.sample.downcase} et rencontrer de nouveaux compagnons.",
-      size: sizes.sample,
-      energy_level: energy_levels.sample,
-      canine_sociability: canine_sociabilities.sample,
-      human_sociability: human_sociabilities.sample,
-      temperament: temperaments.sample,
-      favorite_activity: favorite_activities.sample
+      name: name,
+      breed: breed,
+      gender: ["Male", "Female"].sample,
+      age: rand(1..10),
+      size: ["Petit", "Moyen", "Grand"].sample,
+      energy_level: ["Calme", "Modéré", "Dynamique"].sample,
+      canine_sociability: ["Peu sociable", "Sociable", "Très sociable"].sample,
+      human_sociability: ["Réservé", "Affectueux", "Très affectueux"].sample,
+      temperament: ["Joueur", "Protecteur", "Curieux", "Calme"].sample,
+      favorite_activity: ["Jeux", "Course", "Balade", "Natation"].sample,
+      description: "#{name} est un chien adorable, sociable et plein de personnalité."
     )
+
+    dog.avatar.attach(
+      io: URI.open("https://placedog.net/600/600?id=#{dog_index + 1}"),
+      filename: "#{name.downcase}.jpg",
+      content_type: "image/jpeg"
+    )
+
+    dog_index += 1
   end
 end
 
-puts "Creating sample conversations..."
-
-dog1 = User.first.dogs.first
-dog2 = User.second.dogs.first
-
-conversation = Conversation.create!(
-  dog_one: dog1,
-  dog_two: dog2
-)
-
-Message.create!(
-  conversation: conversation,
-  dog: dog1,
-  content: "Wouf ! On fait une balade ?"
-)
-
-Message.create!(
-  conversation: conversation,
-  dog: dog2,
-  content: "Oui, avec plaisir !"
-)
-
-puts "Creating sample posts..."
-
-post = Post.create!(
-  dog: dog1,
-  content: "Prêt pour rencontrer de nouveaux amis 🐾"
-)
-
-Comment.create!(
-  dog: dog2,
-  post: post,
-  content: "Trop mignon !"
-)
-
-Wouf.create!(
-  dog: dog2,
-  post: post
-)
-
-puts "Done!"
-puts "#{User.count} users created"
-puts "#{Dog.count} dogs created"
-puts "#{Swipe.count} swipes created" if defined?(Swipe)
+puts "#{User.count} utilisateurs créés"
+puts "#{Dog.count} chiens créés"
+puts "Seed terminé !"
