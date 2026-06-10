1099796e35f3256c06003ddd9b5efe4aea005044
Laurent Gardin <laurent.gardin@laposte.net>
2026-06-10 15:28:16 +0200

    Name-conversation

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -3,4 +3,8 @@ class Conversation < ApplicationRecord
   belongs_to :dog_two, class_name: "Dog"
 
   has_many :messages, dependent: :destroy
+
+  def other_dog(current_user)
+    dog_one.user == current_user ? dog_two : dog_one
+  end
 end
