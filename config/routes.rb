4aaad9aef55fb260a6b3af68cb393ad82848a137
Laurent Gardin <laurent.gardin@laposte.net>
2026-06-11 12:24:29 +0200

    Pages errors 404 & 500

--- config/routes.rb
+++ config/routes.rb
@@ -59,4 +59,7 @@ Rails.application.routes.draw do
       get :liked
     end
   end
+
+  match "/404", to: "errors#not_found", via: :all
+  match "/500", to: "errors#internal_server_error", via: :all
 end
